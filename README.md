b06dc2f5871c0ac9129d41e1dbbcdad736627879
Joshua <jdong42@gmail.com>
2016-11-10 04:37:05 -0600

    first working copy

---
+++ README.md
@@ -0,0 +1,27 @@
+SpellingHero
+============
+
+SpellingHero is just someone who wants to get started on open source
+contributions. What better way than by fixing spelling mistakes in
+documentation? SpellingHero scours github for fixes to make, in a similar but
+unrelated fashion to
+[Wikipedia bots](https://en.wikipedia.org/wiki/Wikipedia:Bots). Maybe
+SpellingHero will be the first in a line of Github bots. Who knows?
+
+
+Usage
+-----
+
+I highly encourage the use of [virtual
+envioronments](https://virtualenv.pypa.io/en/stable/). However, user escalating
+to `root` always works in a pinch.
+
+Install dependencies with: `pip install -r requirements.txt`
+
+For good results, you probably want to start with a nice text corpus (even
+though the spell checker gets better with use).  `big.txt` is included as a
+sample corpus, but you should add text files to the `corpus` directory for
+better results. I started out with the [python
+documentation](https://docs.python.org/3/download.html). In the future, I'll
+provide a pickle file in the repo, but the spell checking engine will go
+through major revisions before that.
--- spell.py
+++ spell.py
@@ -1,29 +1,56 @@
 """Statistical spell checker
 See http://norvig.com/spell-correct.html for more details.
 
-TODO: numpy, pickle
+TODO: numpy, pickle, restructure for full documents
 """
 
+import string
 import re
 from collections import Counter
+import glob, os
+import string
+
+CORPUS_DIR = "./corpus"
+
+def _words(text):
+    return re.findall(r'\w+', text.lower())
 
 
-def _words(text): return re.findall(r'\w+', text.lower())
+def _get_corpus():
+    corpus = []
+    with open('big.txt', 'r') as fp:
+        corpus.append(fp.read())
 
-WORDS = Counter(words(open('big.txt').read()))
+    # use all files in the directory
+    for fname in glob.iglob(os.path.join(CORPUS_DIR, '**/*'), recursive=True):
+        if os.path.isfile(fname):
+            with open(fname, 'rb') as fp:
+                # logging.debug("adding to corpus: {}".format(fname))
+                corpus.append(fp.read().decode(errors='ignore'))
+
+    return ''.join(corpus)
+
+WORDS = Counter(_words(_get_corpus()))
 
 
-def _P(word, N=sum(WORDS.values())): 
+def update_corpus(text):
+    WORDS.update(_words(text))
+
+
+def _P(word, N=sum(WORDS.values())):
     "Probability of `word`."
     return WORDS[word] / N
 
 
-def _candidates(word): 
+def _candidates(word):
     "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+    return (_known([word]) or
+            _known(_edits1(word)) or
+            _known(_edits2(word)) or
+            [word])
 
 
-def _known(words): 
+def _known(words):
     "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
 
@@ -39,11 +66,49 @@ def _edits1(word):
     return set(deletes + transposes + replaces + inserts)
 
 
-def _edits2(word): 
+def _edits2(word):
     "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+    return (e2 for e1 in _edits1(word) for e2 in _edits1(e1))
 
 
-def correction(word): 
+def _correction(word):
     "Most probable spelling correction for word."
-    return max(candidates(word), key=P)
+    return max(_candidates(word), key=_P)
+
+
+def _case_match(raw, ref):
+    # if first letter is uppercase
+    if ref[0] in string.ascii_uppercase:
+        raw = raw[0].upper() + raw[1:]
+
+    # if all uppercase
+    if ref[-1] in string.ascii_uppercase:
+        raw = raw.upper()
+
+    return raw
+
+
+def _hil(new, original):
+    u_in = input("{}->{}? ".format(original, new))
+    return u_in and u_in[0] == 'y'
+
+
+def correct(doc):
+    replacements = dict()
+    # naiive word iteration
+    parts = re.split('(\s+)', doc)
+    # sort for debug only
+    for word in sorted(list(set(parts))):
+        # only work with easy words
+        if re.match('^[A-Za-z]+$', word):
+            new_word = _case_match(_correction(word.lower()), word)
+            if new_word != word and _hil(new_word, word):
+                replacements[word] = new_word
+
+    # TODO: very inefficient
+    for i in range(len(parts)):
+        part = parts[i]
+        part = replacements.get(part, part)
+        parts[i] = part
+
+    return ''.join(parts)
